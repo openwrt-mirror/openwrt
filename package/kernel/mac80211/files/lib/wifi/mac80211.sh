ddfbaaea68cabf51d52815f911aeef350e23e232
郭传鈜 <gch981213@gmail.com>
2014-08-04 22:09:11 +0800

    change default ssid style

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -109,7 +109,7 @@ config wifi-iface
 	option device   radio$devidx
 	option network  lan
 	option mode     ap
-	option ssid     OpenWrtBy981213
+	option ssid     OpenWrt-$(cat /sys/class/ieee80211/${dev}/macaddress | awk -F ":" '{print $4""$5""$6 }'| tr a-z A-Z)
 	option encryption none
 	option wds 1
 
