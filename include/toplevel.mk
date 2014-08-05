a12f2554f49634f8a7e26afe8936bccfac5283ed
郭传鈜 <gch981213@gmail.com>
2014-08-05 14:10:47 +0800

    :-)

--- include/toplevel.mk
+++ include/toplevel.mk
@@ -6,7 +6,7 @@
 # See /LICENSE for more information.
 #
 
-RELEASE:=Chaos Calmer
+RELEASE:=Chaos Calmer by 981213
 PREP_MK= OPENWRT_BUILD= QUIET=0
 
 export IS_TTY=$(shell tty -s && echo 1 || echo 0)
