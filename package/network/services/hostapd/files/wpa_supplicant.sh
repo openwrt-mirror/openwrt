b0a4ab92f157051d99187cb324fb1043eb23af6b
郭传鈜 <gch981213@gmail.com>
2014-11-16 14:42:05 +0800

    允许客户端桥接

--- package/network/services/hostapd/files/wpa_supplicant.sh
+++ package/network/services/hostapd/files/wpa_supplicant.sh
@@ -24,10 +24,6 @@ wpa_supplicant_setup_vif() {
 	config_get mode "$vif" mode
 	config_get ifname "$vif" ifname
 	config_get_bool wds "$vif" wds 0
-	[ -z "$bridge" ] || [ "$mode" = ap ] || [ "$mode" = sta -a $wds -eq 1 ] || {
-		echo "wpa_supplicant_setup_vif($ifname): Refusing to bridge $mode mode interface"
-		return 1
-	}
 	[ "$mode" = "adhoc" ] && {
 		modestr="mode=1"
 		scan_ssid="0"
