10f47c47d003ff9625eb77939438b8fe5c53c4b6
郭传鈜 <gch981213@gmail.com>
2014-12-30 23:30:22 +0800

    WPS添加LED指示

--- package/network/services/hostapd/files/wps-hotplug.sh
+++ package/network/services/hostapd/files/wps-hotplug.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
+. /lib/functions/leds.sh
+
+WPS_LED=$(ls /sys/class/leds | grep -E 'qss|wps' | head -n 1)
 
 if [ "$ACTION" = "pressed" -a "$BUTTON" = "wps" ]; then
+	[ -z $WPS_LED ] || led_timer $WPS_LED 150 150
 	for dir in /var/run/hostapd*; do
 		[ -d "$dir" ] || continue
 		hostapd_cli -p "$dir" wps_pbc
 	done
+	sleep 1
+	[ -z $WPS_LED ] || led_off $WPS_LED
 fi
+
