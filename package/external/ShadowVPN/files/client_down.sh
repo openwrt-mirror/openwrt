54e8c95a556308635e18edbff62181f4fdec810f
Syrone Wong <wong.syrone@gmail.com>
2015-02-14 15:39:32 +0800

    Update external packages
    shadowsocks-libev: 2.1.4-4a30b7aa72
    
    redsocks2: 0.60-df76a17549
    
    ShadowVPN: 0.1.6-efe6a47922
    
    Misc scripts updates

--- package/external/ShadowVPN/files/client_down.sh
+++ package/external/ShadowVPN/files/client_down.sh
@@ -9,16 +9,14 @@ loger() {
 	echo "$(date '+%c') down.$1 ShadowVPN[$PID] $2"
 }
 
-# Get original gateway
-loger info "Get the original default gateway"
-gateway=$(ip route show 0/0 | grep via | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}')
-route_mode=$(cat /tmp/shadowvpn/route_mode 2>/dev/null)
+# Get uci setting
+route_mode=$(uci get shadowvpn.@shadowvpn[-1].route_mode_save 2>/dev/null)
 
 # Turn off NAT over VPN
-loger notice "Turn off NAT over $intf"
 iptables -t nat -D POSTROUTING -o $intf -j MASQUERADE
 iptables -D FORWARD -o $intf -j ACCEPT
 iptables -D FORWARD -i $intf -j ACCEPT
+loger notice "Turn off NAT over $intf"
 
 # Change routing table
 ip route del $server
@@ -29,12 +27,11 @@ if [ "$route_mode" != 2 ]; then
 fi
 
 # Remove route rules
-if [ -f /tmp/shadowvpn/routes ]; then
-	sed -e "s/^/route del /" /tmp/shadowvpn/routes |\
-	ip -batch -
+if [ -f /tmp/shadowvpn_routes ]; then
+	sed -e "s/^/route del /" /tmp/shadowvpn_routes | ip -batch -
 	loger notice "Route rules have been removed"
 fi
 
-rm -rf /tmp/shadowvpn
+rm -rf /tmp/shadowvpn_routes
 
 loger info "Script $0 completed"
--- package/external/ShadowVPN/files/client_up.sh
+++ package/external/ShadowVPN/files/client_up.sh
@@ -9,28 +9,29 @@ loger() {
 	echo "$(date '+%c') up.$1 ShadowVPN[$PID] $2"
 }
 
-mkdir -p /tmp/shadowvpn
-
 # Configure IP address and MTU of VPN interface
 ifconfig $intf 10.7.0.2 netmask 255.255.255.0
 ifconfig $intf mtu $mtu
+
+# Get original gateway
+device=$(ip route show 0/0 | grep via | sed -e 's/.* dev \([^ ]*\).*/\1/')
+gateway=$(ip route show 0/0 | grep via | sed -e 's/.* via \([^ ]*\).*/\1/')
+loger info "The default gateway: via $gateway dev $device"
 
 # Get uci setting
 route_mode=$(uci get shadowvpn.@shadowvpn[-1].route_mode 2>/dev/null)
 route_file=$(uci get shadowvpn.@shadowvpn[-1].route_file 2>/dev/null)
 
-# Get original gateway
-loger info "Get the original default gateway"
-gateway=$(ip route show 0/0 | grep via | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}')
-
-# Save current value to file
-echo $route_mode >/tmp/shadowvpn/route_mode
+# Save uci setting
+uci set shadowvpn.@shadowvpn[-1].device_save=$device
+uci set shadowvpn.@shadowvpn[-1].route_mode_save=$route_mode
+uci commit shadowvpn
 
 # Turn on NAT over VPN
-loger notice "Turn on NAT over $intf"
 iptables -t nat -A POSTROUTING -o $intf -j MASQUERADE
 iptables -I FORWARD 1 -o $intf -j ACCEPT
 iptables -I FORWARD 1 -i $intf -j ACCEPT
+loger notice "Turn on NAT over $intf"
 
 # Change routing table
 ip route add $server via $gateway
@@ -45,9 +46,8 @@ fi
 
 # Load route rules
 if [ "$route_mode" != 0 -a -f "$route_file" ]; then
-	grep -E "^([0-9]{1,3}\.){3}[0-9]{1,3}" $route_file >/tmp/shadowvpn/routes
-	sed -e "s/^/route add /" -e "s/$/ $suf/" /tmp/shadowvpn/routes |\
-	ip -batch -
+	grep -E "^([0-9]{1,3}\.){3}[0-9]{1,3}" $route_file >/tmp/shadowvpn_routes
+	sed -e "s/^/route add /" -e "s/$/ $suf/" /tmp/shadowvpn_routes | ip -batch -
 	loger notice "Route rules have been loaded"
 fi
 
