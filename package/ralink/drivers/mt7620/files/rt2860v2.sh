487e6987463864c678130d838ef08bd05690b239
郭传鈜 <gch981213@gmail.com>
2015-03-01 12:04:08 +0800

    Use the newest rt2860v2.sh from lintel

--- package/ralink/drivers/mt7620/files/rt2860v2.sh
+++ package/ralink/drivers/mt7620/files/rt2860v2.sh
@@ -1,14 +1,107 @@
 #!/bin/sh
+# Copyright (c) 2013 OpenWrt
+# Copyright (C) 2014 D-Team Technology Co.,Ltd. ShenZhen
+# Copyright (c) 2005-2014, lintel <lintel.huang@gmail.com>
+# Copyright (c) 2013, Hoowa <hoowa.sun@gmail.com>
 #
-# by lintel@gmail.com, hoowa.sun@gmail.com
+# 	描述:RT2860v2/MT7610E/MT7612E uci无线脚本
+#	功能：对接uci与rt2860v2配置文件，
 #
-
+#     警告:对着屏幕的哥们,我们允许你使用此脚本，但不允许你抹去作者的信息,请保留这段话。
+#
+ 
 append DRIVERS "rt2860v2"
 
-prepare_config() {
-#获取参数 存储配置的变量 目标配置关键字
+debug() {
+ # echo "debug: ""$1"
+  echo -n
+}
 
-	local num=0 mode disabled
+#修复无法重置raX虚拟接口问题。
+reload_rt2860v2() {
+	debug "reload_rt2860v2"
+	local vif
+	for vif in ra0 ra1 ra2 ra3 ra4 ra5 ra6 ra7 wds0 wds1 wds2 wds3 apcli0; do
+	ifconfig $vif down 2>/dev/null
+	set_wifi_down $vif
+	#debug "ifdown $vif"
+	done
+	rmmod rt2860v2_ap 2>/dev/null
+	insmod rt2860v2_ap 2>/dev/null
+	
+	killall apctrl >/dev/null
+}
+
+scan_rt2860v2() {
+	debug "scan_rt2860v2"
+	debug "$1 $2 $3"
+	local device="$1"
+	local ifname="$1"
+
+	rt2860v2_phy_if=0
+	
+	#重置整个驱动
+#	reload_rt2860v2
+
+	local i=-1
+	
+	while grep -qs "^ *ra$((++i)):" /proc/net/dev; do
+	debug "found phy$i ra$i inteface"
+	let rt2860v2_phy_if+=1
+	done
+
+	rt2860v2_ap_num=0
+	rt2860v2_wds_num=0
+	rt2860v2_sta_num=0
+	rt2860v2_sta_first=0
+	
+	config_get vifs "$device" vifs
+	
+	for vif in $vifs; do
+		config_get_bool disabled "$vif" disabled 0
+		[ $disabled -eq 0 ] || continue
+
+		config_get mode "$vif" mode
+		case "$mode" in
+			adhoc)
+				echo "rt2860v2 only support ap+wds+sta!"
+			;;
+			sta)
+				[ ${rt2860v2_sta_num:-0} -ge 1 ] && {
+					echo "rt2860v2 only support 1 sta!"
+				}
+				rt2860v2_sta_num=$(($rt2860v2_sta_num + 1))
+				debug "rt2860v2_sta_num=$rt2860v2_sta_num"
+			;;
+			ap)
+				rt2860v2_ap_num=$(($rt2860v2_ap_num + 1))
+				debug "rt2860v2_ap_num=$rt2860v2_ap_num"
+				apmode=1
+				rt2860v2_ap_vif="${rt2860v2_ap_vif:+$rt2860v2_ap_vif }$vif"
+			;;
+			wds)
+				config_get wds_addr "$vif" bssid
+				[ -z "$wds_addr" ] || {
+					wds_addr=$(echo "$wds_addr" | tr 'A-F' 'a-f')
+					append rt2860v2_wds "$wds_addr"
+				}
+			;;
+			monitor)
+			;;
+			*) echo "$device($vif): Invalid mode";;
+		esac
+	done
+	#开始准备该设备的无线配置参数
+	rt2860v2_prepare_config $device
+}
+
+
+rt2860v2_prepare_config() {
+
+	debug "rt2860v2_prepare_config"
+	
+	#获取参数 存储配置的变量 目标配置关键字
+	local ssid_num=0 apcli_num=0 mode disabled
 	
 #准备产生RaX的无线配置
 	local device=$1
@@ -38,6 +131,7 @@ prepare_config() {
 	config_get maclist $device maclist
 #字符格式转义
 	ra_maclist="${maclist// /;};"
+	
 #是否支持GREEN AP功能
 	config_get_bool greenap $device greenap 0
 
@@ -47,35 +141,45 @@ prepare_config() {
 	
 	config_get rts "$device" rts 2347
 	
+	config_get txburst "$device" txburst 0
+
 	config_get distance "$device" distance
 
 	config_get hidessid "$device" hidden 0
 	
 #获取该Radio下面的虚拟接口	
 	config_get vifs "$device" vifs
-	
+
+#大于11的时候支持11-14号频道
+	[ "$channel" != "auto" ] && {
+	[ ${channel:-0} -ge 1 -a ${channel:-0} -le 11 ] && countryregion=0
+	[ ${channel:-0} -ge 12 -a ${channel:-0} -le 13 ] && countryregion=1
+	[ ${channel:-0} -eq 14 ] && countryregion=31
+	debug "channel=$channel countryregion=$countryregion"
+	}
+
 #获取虚拟接口的数量，并提前配置SSID
 for vif in $vifs; do
-	let num+=1
 	config_get_bool disabled "$vif" disabled 0
 	config_get mode "$vif" mode 0
+	
+	#如果关闭，则略过该vif配置
+	[ "$disabled" == "1" ]&& {
+	set_wifi_down $vif
+	continue
+	}
 	
 	#如果某个SSID接口需要隐藏，则所有的接口都隐藏
 	[ "$hidessid" == "0" ] && {
 	config_get hidessid $vif hidden 0
 	}
 	
-	#已经关闭的接口以及sta模式的排除在外。
-	[ "$mode" = "sta" ]&& {
-	let num-=1 
+	[ "$mode" == "sta" ] || [ "$mode" == "wds" ] && {
 	continue
 	}
-	[ "$disabled" == "1" ]&& {
-	let num-=1
-	continue
-	}
-	
-	case $num in
+
+	let ssid_num+=1 #SSID Number
+	case $ssid_num in
 	1)
 		config_get ssid1 "$vif" ssid
 		;;
@@ -89,6 +193,7 @@ for vif in $vifs; do
 		config_get ssid4 "$vif" ssid
 		;;
 	*)
+		echo "Only support 4 MSSID!"
 		;;
 	esac
 done
@@ -106,24 +211,24 @@ done
       HT=1 
       HT_CE=0
     else
-    echo "ht config has some problem!use default!!!"
+    echo "ht config error!use default!!!"
       HT=0
       HT_CE=1
     fi
 
 
-	# 在HT40模式下,需要另外的一个频道，如果EXTCHA=0,则当前第二频道为$channel + 4.
-	# 如果EXTCHA=1,则当前的第二频道为$channel - 4.
+	# 在HT40模式下,需要另外的一个频道，如果EXTCHA=1,则当前第二频道为$channel + 4.
+	# 如果EXTCHA=0,则当前的第二频道为$channel - 4.
 	# 如果当前频道被限制在1-4,则是当前频道+ 4，若否，则为当前频道-4 
 	
 	EXTCHA=1
 	
-	[ "$channel" != auto ] && [ "$channel" -lt "5" ] && EXTCHA=1
+	[ "$channel" != "auto" ] && [ "$channel" -lt "5" ] && EXTCHA=0
 
 #配置自动选择无线频道
     [ "$channel" == "auto" ] && {
         channel=11
-        AutoChannelSelect=2
+        AutoChannelSelect=2 #增强型自动频道选择
     }
 
 #开始判断WiFi的MAC过滤方式.
@@ -139,13 +244,18 @@ done
 	;;
     esac
 
+	#FIXME:单独的STA模式无法启动。
+	[ "$rt2860v2_ap_num" = "0" ] && [ "$rt2860v2_sta_num" = "1" ] && {
+	 ssid_num=1
+	}
+					
 	cat > /tmp/RT2860.dat<<EOF
 #The word of "Default" must not be removed
 Default
-CountryRegion=0
+CountryRegion=${countryregion:-0}
 CountryRegionABand=7
 CountryCode=${country:-US}
-BssidNum=${num:-1}
+BssidNum=${ssid_num:-1}
 SSID1=${ssid1:-PandoraBox_SSID1}
 SSID2=${ssid2:-PandoraBox_SSID2}
 SSID3=${ssid3:-PandoraBox_SSID3}
@@ -169,7 +279,7 @@ RxAntenna=
 TxPreamble=1
 RTSThreshold=${rts:-2347}
 FragThreshold=${frag:-2346}
-TxBurst=1
+TxBurst=${txburst:-0}
 PktAggregate=1
 AutoProvisionEn=0
 FreqDelta=0
@@ -330,11 +440,18 @@ HT_AMSDU=0
 HT_BAWinSize=64
 HT_GI=1
 HT_STBC=1
+HT_LDPC=0
 HT_MCS=33
+VHT_BW=1
+VHT_SGI=1
+VHT_STBC=0
+VHT_BW_SIGNAL=0
+VHT_DisallowNonVHT=0
+VHT_LDPC=0
 HT_TxStream=2
 HT_RxStream=2
 HT_PROTECT=1
-HT_DisallowTKIP=1
+HT_DisallowTKIP=0
 HT_BSSCoexistence=${HT_CE:-1}
 GreenAP=${greenap:-0}
 WscConfMode=0
@@ -407,27 +524,41 @@ Key1Str=
 Key2Str=
 Key3Str=
 Key4Str=
+WscManufacturer=PandoraBox
+WscModelName=PandoraBox Wireless Router
+WscDeviceName=PandoraBox 2.4G WiFI
+WscModelNumber=
+WscSerialNumber=
+PMFMFPC=0
+PMFMFPR=0
+PMFSHA256=0
+LoadCodeMethod=0
+AutoChannelSkipList=12;13;14;
 EOF
 
 }
-
-reload_rt2860v2() {
-	ifconfig ra0 down
-	rmmod rt2860v2_ap 
-
-	insmod rt2860v2_ap
-	ifconfig ra0 up
-}
-
-scan_rt2860v2() {
-	local device="$1"
-}
-
 disable_rt2860v2() {
-	local device="$1"
-	set_wifi_down $device
-	ifconfig $device down
+	debug "disable_rt2860v2"
+	local vif
+	for vif in ra0 ra1 ra2 ra3 ra4 ra5 ra6 ra7 wds0 wds1 wds2 wds3 apcli0; do
+	ifconfig $vif down 2>/dev/null
+	set_wifi_down $vif
+	#debug "ifdown $vif"
+	done
+	
 	true
+}
+
+enable_rt2860v2_wps_pbc() {
+#帮你解决问题了，你该怎么做? By Lintel 2014.05.05
+#注册 代理 中继
+iwpriv $1 set WscConfMode=7 
+#WPS AP未配置
+iwpriv $1 set WscConfStatus=2
+#WPS PBC模式
+iwpriv $1 set WscMode=2
+#WPS WSC V2支持
+iwpriv $1 set WscV2Support=0
 }
 
 rt2860v2_start_vif() {
@@ -442,56 +573,111 @@ rt2860v2_start_vif() {
 }
 
 enable_rt2860v2() {
-#传参过来的第一个参数是Radio0
-	local device="$1" dmode if_num=0;
 	
+	#传参过来的第一个参数是device
+	local device="$1" dmode if_num=-1;
+	debug "enable_rt2860v2"
+	
+	if [ $(is_mt7621) == "1" ]; then
+	{
+		#检查并创建WiFi驱动配置链接
+		[ -f /etc/Wireless/MT76X2/MT7602.dat ] || {
+		mkdir -p /etc/Wireless/MT76X2/ 2>/dev/null
+		touch /tmp/RT2860.dat
+		ln -s /tmp/RT2860.dat /etc/Wireless/MT76X2/MT7602.dat 2>/dev/null
+		}
+	}
+	else
+	{
+		#检查并创建WiFi驱动配置链接
+		[ -f /etc/Wireless/RT2860/RT2860.dat ] || {
+		mkdir -p /etc/Wireless/RT2860/ 2>/dev/null
+		touch /tmp/RT2860.dat
+		ln -s /tmp/RT2860.dat /etc/Wireless/RT2860/RT2860.dat 2>/dev/null
+		}
+	}
+	fi;
+	
+	#重置整个驱动
+	reload_rt2860v2
+	debug "rt2860v2_ap_num=$rt2860v2_ap_num"
 	config_get_bool disabled "$device" disabled 0	
 	if [ "$disabled" = "1" ] ;then
-	ifconfig ra0 down
 	return
 	fi
 	
 	#开始准备该设备的无线配置参数
-	prepare_config $device
+#	rt2860v2_prepare_config $device
 	
 	config_get dmode $device mode
 	config_get vifs "$device" vifs
 
 	config_get maxassoc $device maxassoc 0
-
+	
+	local rt2860v2_vifs rt2860v2_ap_vifs  rt2860v2_sta_vifs rt2860v2_wds_vifs
+	
 	for vif in $vifs; do
+		debug "vifs vif=$vif"
+		config_get mode "$vif" mode
+		[ "$mode" == "ap" ] && {
+			rt2860v2_ap_vifs="$rt2860v2_ap_vifs $vif"
+		}
+		[ "$mode" == "sta" ] && {
+			rt2860v2_sta_vifs="$rt2860v2_sta_vifs $vif"
+		}
+		[ "$mode" == "wds" ] && {
+			rt2860v2_wds_vifs="$rt2860v2_ap_vifs $vif"
+		}
+	done
+	
+	rt2860v2_vifs="$rt2860v2_vifs$rt2860v2_ap_vifs $rt2860v2_sta_vifs $rt2860v2_wds_vifs "
+	
+#	for vif in $vifs; do
+	for vif in $rt2860v2_vifs; do
+		config_get_bool disabled $vif disabled 0
+		#如果关闭，则略过该vif配置
+		[ "$disabled" == "1" ] && {
+			continue
+		}
+		
 		local ifname encryption key ssid mode
 		
-		config_get ifname $vif device	
-		
-		#根据ifname数量配置多SSID
-		[ "$ifname" == "ra0" ] && {
-		ifname="ra$if_num"
-		}
-		let if_num+=1
-		#排除如果设置为apcli0
-		[ "$mode" = "sta" ]&& {let if_num-=1}
-		
-		config_get_bool disabled $vif disabled 0
-		if [ "$disabled" = "1" ] ;then
-		set_wifi_down $ifname
-		echo "Interface $ifname disabled"
-		return
-		fi
+#		config_get ifname $vif device			
 		config_get encryption $vif encryption
 		config_get key $vif key
 		config_get ssid $vif ssid
 		config_get mode $vif mode
-		config_get wps $vif wps
+		
+		#获取WPS配置信息
+		local wps pin
+		config_get wps $vif wps 
+		config_get pin $vif pin
+		
 		#是否隔离客户端
 		config_get isolate $vif isolate 0
+		
 		#802.11h
 		config_get doth $vif doth 0
-		#是否隐藏SSID
-#		config_get hidessid $vif hidden 0
 
+#		config_get hidessid $vif hidden 0	
+
+		#排除如果设置为sta wds
+		[ "$mode" != "sta" ] && [ "$mode" != "wds" ] && {
+		let if_num+=1
+		debug "if_num=$if_num"
+		
+		#根据ifname数量配置多SSID
+		ifname="ra$if_num"
+		}
+		
 		#STA APClient配置
 		[ "$mode" == "sta" ] && {
+
+					[ "$rt2860v2_ap_num" = "0" ] && [ "$rt2860v2_sta_num" = "1" ] || [ "$if_num" == "-1" ]&& {
+						#初始化phy
+						ifconfig ra0 up
+					}
+					
 					#如果为apcli模式，指定接口名称为apcli0
 					ifname="apcli0"
 					echo "#Encryption" >/tmp/wifi_encryption_${ifname}.dat
@@ -531,10 +717,14 @@ enable_rt2860v2() {
 					;;
 			esac
 					iwpriv $ifname set ApCliEnable=1
-					ifconfig $ifname up
+#					ifconfig $ifname up
+					#FIXME:单独STA模式
+					[ "$rt2860v2_ap_num" = "0" ] && [ "$rt2860v2_sta_num" = "1" ] && {
+						ifconfig ra0 down
+					}
 		}
 		#AP模式配置
-		[ "$mode" == "sta" ] || {
+		[ "$mode" == "ap" ] && {
 			[ "$key" = "" -a "$vif" = "private" ] && {
 				logger "no key set serial"
 				key="AAAAAAAAAA"
@@ -582,16 +772,25 @@ enable_rt2860v2() {
 					iwpriv $ifname set "WPAPSK=${key}"
 					iwpriv $ifname set DefaultKeyID=2
 					iwpriv $ifname set "SSID=${ssid}"
-						
-					if [ "DefaultKeyID=2$wps" == "1" ]; then
-						iw"SSID=${ssid}"priv $ifname set WscConfMode=7
+					
+					#配置WPS	
+					if [ "$wps" == "pbc" ]  && [ "$encryption" != "none" ]; then {
+					enable_rt2860v2_wps_pbc $ifname
+					touch /tmp/"$ifname"_wps_pbc.lock 2>/dev/null
+					}
 					else
-						iwpriv $ifname set WscConfMode=0
-					fi
+					rm -rf /tmp/"$ifname"_wps_pbc.lock 2>/dev/null
+					fi;
+					
 					;;
+					
 				WEP|wep|wep-open|wep-shared)
 					echo "WEP" >>/tmp/wifi_encryption_${ifname}.dat
-					iwpriv $ifname set AuthMode=WEPAUTO
+					if [ "$encryption" == "wep-shared" ]; then
+						iwpriv $ifname set AuthMode=SHARED                  
+					else  
+						iwpriv $ifname set AuthMode=WEPAUTO
+					fi
 					iwpriv $ifname set EncrypType=WEP
 					iwpriv $ifname set IEEE8021X=0
 					for idx in 1 2 3 4; do
@@ -606,8 +805,8 @@ enable_rt2860v2() {
 				none|open)
 					echo "NONE" >>/tmp/wifi_encryption_${ifname}.dat
 					iwpriv $ifname set AuthMode=OPEN
-					iwpriv $ifname set WscConfMode=0
 					iwpriv $ifname set EncrypType=NONE
+					iwpriv $ifname set WscConfMode=0
 					;;
 			esac
 		}
@@ -619,14 +818,7 @@ enable_rt2860v2() {
 		else
 		 iwpriv $ifname set RadioOn=1
 		fi
-
-		#检查是否需要进行SSID隐藏。
-#		if [ $hidessid == "1" ]; then
-#		 iwpriv $ifname set HideSSID=1
-#		else
-#		 iwpriv $ifname set HideSSID=0
-#		fi
-
+		
 		#隔离客户端连接。
 		[ $isolate == "1" ]&&{
 			iwpriv $ifname set NoForwarding=1
@@ -664,310 +856,85 @@ enable_rt2860v2() {
 
 		}
 		fi;
-		set_wifi_up "$vif" "$ifname"
-
-		# If isolation is requested, disable forwarding between
-		# wireless clients (both within the same BSSID and
-		# between BSSID's, though the latter is probably not
-		# relevant for our setup).
 		
-#		iwpriv $ifname set NoForwarding="${isolate:-0}"
-#		iwpriv $ifname set NoForwardingBTNBSSID="${isolate:-0}"
-
+		set_wifi_up "$vif" "$ifname"
+		debug "set vif=$vif $ifname up"
 	done
 	
 	#配置无线最大连接数
 	iwpriv $device set MaxStaNum=$maxassoc
 }
 
-first_enable() {
-
-ifconfig ra0 down
-
-	cat > /tmp/RT2860.dat<<EOF
-#The word of "Default" must not be removed
-Default
-CountryRegion=0
-CountryRegionABand=7
-CountryCode=US
-BssidNum=1
-SSID1=PandoraBox
-WirelessMode=9
-FixedTxMode=
-TxRate=0
-Channel=11
-BasicRate=15
-BeaconPeriod=100
-DtimPeriod=1
-TxPower=100
-DisableOLBC=0
-BGProtection=0
-TxAntenna=
-RxAntenna=
-TxPreamble=1
-RTSThreshold=2347
-FragThreshold=2346
-TxBurst=1
-PktAggregate=1
-AutoProvisionEn=0
-FreqDelta=0
-TurboRate=0
-WmmCapable=1
-APAifsn=3;7;1;1
-APCwmin=4;4;3;2
-APCwmax=6;10;4;3
-APTxop=0;0;94;47
-APACM=0;0;0;0
-BSSAifsn=3;7;2;2
-BSSCwmin=4;4;3;2
-BSSCwmax=10;10;4;3
-BSSTxop=0;0;94;47
-BSSACM=0;0;0;0
-AckPolicy=0;0;0;0
-APSDCapable=0
-DLSCapable=0
-NoForwarding=0
-NoForwardingBTNBSSID=0
-HideSSID=1
-ShortSlot=1
-AutoChannelSelect=0
-IEEE8021X=0
-IEEE80211H=0
-CarrierDetect=0
-ITxBfEn=0
-PreAntSwitch=
-PhyRateLimit=0
-DebugFlags=0
-ETxBfEnCond=0
-ITxBfTimeout=0
-ETxBfTimeout=0
-ETxBfNoncompress=0
-ETxBfIncapable=0
-FineAGC=0
-StreamMode=0
-StreamModeMac0=
-StreamModeMac1=
-StreamModeMac2=
-StreamModeMac3=
-CSPeriod=6
-RDRegion=
-StationKeepAlive=0
-DfsLowerLimit=0
-DfsUpperLimit=0
-DfsOutdoor=0
-SymRoundFromCfg=0
-BusyIdleFromCfg=0
-DfsRssiHighFromCfg=0
-DfsRssiLowFromCfg=0
-DFSParamFromConfig=0
-FCCParamCh0=
-FCCParamCh1=
-FCCParamCh2=
-FCCParamCh3=
-CEParamCh0=
-CEParamCh1=
-CEParamCh2=
-CEParamCh3=
-JAPParamCh0=
-JAPParamCh1=
-JAPParamCh2=
-JAPParamCh3=
-JAPW53ParamCh0=
-JAPW53ParamCh1=
-JAPW53ParamCh2=
-JAPW53ParamCh3=
-FixDfsLimit=0
-LongPulseRadarTh=0
-AvgRssiReq=0
-DFS_R66=0
-BlockCh=
-GreenAP=0
-PreAuth=0
-AuthMode=OPEN
-EncrypType=NONE
-WapiPsk1=0123456789
-WapiPsk2=
-WapiPsk3=
-WapiPsk4=
-WapiPsk5=
-WapiPsk6=
-WapiPsk7=
-WapiPsk8=
-WapiPskType=0
-Wapiifname=
-WapiAsCertPath=
-WapiUserCertPath=
-WapiAsIpAddr=
-WapiAsPort=
-RekeyMethod=DISABLE
-RekeyInterval=3600
-PMKCachePeriod=10
-MeshAutoLink=0
-MeshAuthMode=
-MeshEncrypType=
-MeshDefaultkey=0
-MeshWEPKEY=
-MeshWPAKEY=
-MeshId=
-WPAPSK1=12345678
-WPAPSK2=
-WPAPSK3=
-WPAPSK4=
-WPAPSK5=
-WPAPSK6=
-WPAPSK7=
-WPAPSK8=
-DefaultKeyID=1
-Key1Type=0
-Key1Str1=
-Key1Str2=
-Key1Str3=
-Key1Str4=
-Key1Str5=
-Key1Str6=
-Key1Str7=
-Key1Str8=
-Key2Type=0
-Key2Str1=
-Key2Str2=
-Key2Str3=
-Key2Str4=
-Key2Str5=
-Key2Str6=
-Key2Str7=
-Key2Str8=
-Key3Type=0
-Key3Str1=
-Key3Str2=
-Key3Str3=
-Key3Str4=
-Key3Str5=
-Key3Str6=
-Key3Str7=
-Key3Str8=
-Key4Type=0
-Key4Str1=
-Key4Str2=
-Key4Str3=
-Key4Str4=
-Key4Str5=
-Key4Str6=
-Key4Str7=
-Key4Str8=
-HSCounter=0
-HT_HTC=1
-HT_RDG=1
-HT_LinkAdapt=0
-HT_OpMode=0
-HT_MpduDensity=5
-HT_EXTCHA=0
-HT_BW=0
-HT_AutoBA=1
-HT_BADecline=0
-HT_AMSDU=0
-HT_BAWinSize=64
-HT_GI=1
-HT_STBC=1
-HT_MCS=33
-HT_TxStream=2
-HT_RxStream=2
-HT_PROTECT=1
-HT_DisallowTKIP=1
-HT_BSSCoexistence=1
-GreenAP=0
-WscConfMode=0
-WscConfStatus=1
-WCNTest=0
-AccessPolicy0=0
-AccessControlList0=
-AccessPolicy1=0
-AccessControlList1=
-AccessPolicy2=0
-AccessControlList2=
-AccessPolicy3=0
-AccessControlList3=
-AccessPolicy4=0
-AccessControlList4=
-AccessPolicy5=0
-AccessControlList5=
-AccessPolicy6=0
-AccessControlList6=
-AccessPolicy7=0
-AccessControlList7=
-WdsEnable=0
-WdsPhyMode=
-WdsEncrypType=NONE
-WdsList=
-Wds0Key=
-Wds1Key=
-Wds2Key=
-Wds3Key=
-RADIUS_Server=
-RADIUS_Port=1812
-RADIUS_Key1=
-RADIUS_Key2=
-RADIUS_Key3=
-RADIUS_Key4=
-RADIUS_Key5=
-RADIUS_Key6=
-RADIUS_Key7=
-RADIUS_Key8=
-RADIUS_Acct_Server=
-RADIUS_Acct_Port=1813
-RADIUS_Acct_Key=
-own_ip_addr=
-Ethifname=
-EAPifname=
-PreAuthifname=
-session_timeout_interval=0
-idle_timeout_interval=0
-WiFiTest=0
-TGnWifiTest=0
-ApCliEnable=0
-ApCliSsid=
-ApCliBssid=
-ApCliAuthMode=
-ApCliEncrypType=
-ApCliWPAPSK=
-ApCliDefaultKeyID=0
-ApCliKey1Type=0
-ApCliKey1Str=
-ApCliKey2Type=0
-ApCliKey2Str=
-ApCliKey3Type=0
-ApCliKey3Str=
-ApCliKey4Type=0
-ApCliKey4Str=
-RadioOn=1
-SSID=
-WPAPSK=0123456789
-Key1Str=
-Key2Str=
-Key3Str=
-Key4Str=
-EOF
-
-ifconfig ra0 up
+#获取MAC地址
+rt2860v2_get_mac() {
+#	/lib/functions.sh
+	factory_part=$(find_mtd_part $1)
+	dd bs=1 skip=4 count=6 if=$factory_part 2>/dev/null | /usr/sbin/maccalc bin2mac	
 }
 
+is_mt7621() {
+	local cpu_name
+	cpu_name=$(awk 'BEGIN{FS="[ \t]+:[ \t]"} /system type/ {print $2}' /proc/cpuinfo)
+	
+	case "$cpu_name" in
+	*"MediaTek MT7621")
+		echo "1"
+		;;
+	*)
+		echo "0"
+		;;
+	esac
+}
 #detect_rt2860v2函数用于检测是否存在驱动
 detect_rt2860v2() {
 	local i=-1
+
 #判断系统是否存在rt2860v2_ap，不存在则退出
 	cd /sys/module/
-	[ -d rt2860v2_ap ] || return
+
+	if [ $(is_mt7621) == "1" ]; then
+	{
+		[ -d mt76x2e ] && [ -d mt7603e ] && {
+		#检查并创建WiFi驱动配置链接
+		[ -f /etc/Wireless/MT76X2/MT7603.dat ] || {
+		mkdir -p /etc/Wireless/MT7603/ 2>/dev/null
+		touch /tmp/RT2860.dat
+		ln -s /tmp/RT2860.dat /etc/Wireless/MT7603/MT7603.dat 2>/dev/null
+		}  } || {
+		#检查并创建WiFi驱动配置链接
+		[ -f /etc/Wireless/MT76X2/MT7602.dat ] || {
+		mkdir -p /etc/Wireless/MT76X2/ 2>/dev/null
+		touch /tmp/RT2860.dat
+		ln -s /tmp/RT2860.dat /etc/Wireless/MT76X2/MT7602.dat 2>/dev/null
+		} }
+	}
+	else
+	{
+		[ -d rt2860v2_ap ] || return
+		#检查并创建WiFi驱动配置链接
+		[ -f /etc/Wireless/RT2860/RT2860.dat ] || {
+		mkdir -p /etc/Wireless/RT2860/ 2>/dev/null
+		touch /tmp/RT2860.dat
+		ln -s /tmp/RT2860.dat /etc/Wireless/RT2860/RT2860.dat 2>/dev/null
+		}
+	}
+	fi;
+	
+#检查并创建WiFi驱动配置链接
+	[ -f /etc/Wireless/RT2860/RT2860.dat ] || {
+	mkdir -p /etc/Wireless/RT2860/ 2>/dev/null
+	touch /tmp/RT2860.dat
+	ln -s /tmp/RT2860.dat /etc/Wireless/RT2860/RT2860.dat 2>/dev/null
+	}
+
 #检测系统存在多少个wifi接口
 	while grep -qs "^ *ra$((++i)):" /proc/net/dev; do
 		config_get type ra${i} type
 		[ "$type" = rt2860v2 ] && continue
 		
-#检查并创建WiFi驱动配置链接
-	[ -f /etc/Wireless/RT2860/RT2860.dat ] || {
-	mkdir -p /etc/Wireless/RT2860/
-	ln -s /tmp/RT2860.dat /etc/Wireless/RT2860/RT2860.dat
-	}
-	
-	first_enable
+	local pre_wpa2_key
+	pre_wpa2_key=$(hexdump -n 8 /dev/urandom |awk '{print $2$3$4$5;}')
+#	rt2860v2_mac=$(rt2860v2_get_mac Factory)	
 	
 		cat <<EOF
 config wifi-device  ra${i}
@@ -975,22 +942,24 @@ config wifi-device  ra${i}
 	option mode 	9
 	option channel  auto
 	option txpower 100
-	option ht 20+40
-    	option country US
-# REMOVE THIS LINE TO ENABLE WIFI:
-	option disabled 0	
+	option ht 	20+40
+	option country US
 	
 config wifi-iface
 	option device   ra${i}
 	option network	lan
 	option mode     ap
-	option ssid     PandoraBox${i#0}_$(cat /sys/class/net/ra${i}/address|awk -F ":" '{print $4""$5""$6 }'| tr a-z A-Z)
+	option wps     pbc
+	option ssid     PandoraBox${i#0}_$( echo $(rt2860v2_get_mac factory) | awk -F ":" '{print $4""$5""$6 }'| tr a-z A-Z)
 	option encryption none
+#	option encryption psk2
+#	option key $pre_wpa2_key
+	
+	
 EOF
 
-	ifconfig ra0 down 
 	done
-	
+
 }
 
 
