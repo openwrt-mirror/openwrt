b9d851dc39bcb6446e06ac6ce2e7d1b6f1b6819d
Syrone Wong <wong.syrone@gmail.com>
2015-04-26 17:22:57 +0800

    ar71xx: tl-wr2041n-v1: misc
    wlan led controlled by wmac.

--- target/linux/ar71xx/files/arch/mips/ath79/mach-tl-wr2041n-v1.c
+++ target/linux/ar71xx/files/arch/mips/ath79/mach-tl-wr2041n-v1.c
@@ -1,5 +1,6 @@
 /*
  *  TP-LINK TL-WR2041N v1 board support
+ *  Board Info: CPU：ar9344   550MHZ  wireless:ar9381
  *
  *  Copyright (C) 2012 Gabor Juhos <juhosg@openwrt.org>
  *  Copyright (C) 2013 Gui Iribarren <gui@altermundi.net>
@@ -27,18 +28,27 @@
 #include "dev-spi.h"
 #include "dev-wmac.h"
 #include "machtypes.h"
+/* TODO:
+ * How to initial AR9381 via pci, WLAN led mostly drives by wmac.
+ * Unknown external PA and LNA GPIO, maybe we can refer to Qihoo C301, already disabled JTAG.
+ * Turbo button and led need driver.
+ */
+#define WR2041N_GPIO_LED_TURBO		12
 
-#define WR2041N_GPIO_LED_WLAN		13
+/* The WLAN LEDs use GPIOs on the discrete AR9381 wmac ( single 2.4g version of AR9380 ) */
+/* this value is not confirmed at this point */
+#define WR2041N_GPIO_WMAC_LED_WLAN	13
 #define WR2041N_GPIO_LED_SYSTEM		14
 #define WR2041N_GPIO_LED_QSS		15
-#define WR2041N_GPIO_LED_WAN		18
-#define WR2041N_GPIO_LED_LAN4		19
-#define WR2041N_GPIO_LED_LAN3		20
-#define WR2041N_GPIO_LED_LAN2		21
-#define WR2041N_GPIO_LED_LAN1		22
 
-#define WR2041N_GPIO_BTN_WPS		16
-#define WR2041N_GPIO_BTN_RFKILL		17
+#define WR2041N_GPIO_LED_WAN		18
+#define WR2041N_GPIO_LED_LAN1		19
+#define WR2041N_GPIO_LED_LAN2		20
+#define WR2041N_GPIO_LED_LAN3		21
+#define WR2041N_GPIO_LED_LAN4		22
+
+#define WR2041N_GPIO_BTN_RESET		16
+#define WR2041N_GPIO_BTN_TURBO		17
 
 #define WR2041N_KEYS_POLL_INTERVAL	20	/* msecs */
 #define WR2041N_KEYS_DEBOUNCE_INTERVAL	(3 * WR2041N_KEYS_POLL_INTERVAL)
@@ -68,49 +78,33 @@ static struct gpio_led wr2041n_leds_gpio[] __initdata = {
 		.gpio		= WR2041N_GPIO_LED_SYSTEM,
 		.active_low	= 1,
 	},
-        {
+	{
 		.name		= "tp-link:green:wlan",
-		.gpio		= WR2041N_GPIO_LED_WLAN,
-		.active_low	= 1,
-	},
-        {
-		.name		= "tp-link:green:lan1",
-		.gpio		= WR2041N_GPIO_LED_LAN1,
-		.active_low	= 1,
-	},
-        {
-		.name		= "tp-link:green:lan2",
-		.gpio		= WR2041N_GPIO_LED_LAN2,
-		.active_low	= 1,
-	},
-        {
-		.name		= "tp-link:green:lan3",
-		.gpio		= WR2041N_GPIO_LED_LAN3,
-		.active_low	= 1,
-	},
-        {
-		.name		= "tp-link:green:lan4",
-		.gpio		= WR2041N_GPIO_LED_LAN4,
+		.gpio		= WR2041N_GPIO_WMAC_LED_WLAN,
 		.active_low	= 1,
 	},
 };
 
 static struct gpio_keys_button wr2041n_gpio_keys[] __initdata = {
 	{
-		.desc		= "QSS button",
+		.desc		= "Reset button",
 		.type		= EV_KEY,
-		.code		= KEY_WPS_BUTTON,
+		.code		= KEY_RESTART,
 		.debounce_interval = WR2041N_KEYS_DEBOUNCE_INTERVAL,
-		.gpio		= WR2041N_GPIO_BTN_WPS,
+		.gpio		= WR2041N_GPIO_BTN_RESET,
 		.active_low	= 1,
 	},
+/* refer to: http://lxr.free-electrons.com/source/include/linux/gpio_keys.h?v=3.18#L21 
+ * We dont know how to use it.
+
 	{
-		.desc		= "RFKILL switch",
+		.desc		= "Turbo button",
 		.type		= EV_SW,
-		.code		= KEY_RFKILL,
+		.code		= KEY_DUMMY,
 		.debounce_interval = WR2041N_KEYS_DEBOUNCE_INTERVAL,
-		.gpio		= WR2041N_GPIO_BTN_RFKILL,
+		.gpio		= WR2041N_GPIO_BTN_TURBO,
 	},
+ */
 };
 
 
@@ -121,6 +115,8 @@ static void __init wr2041n_setup(void)
 	u8 tmpmac[ETH_ALEN];
 
 	ath79_register_m25p80(&wr2041n_flash_data);
+	/* Disable JTAG to enable GPIO 0-4 */
+	ath79_gpio_function_enable(AR934X_GPIO_FUNC_JTAG_DISABLE);
 
 	ath79_register_leds_gpio(-1, ARRAY_SIZE(wr2041n_leds_gpio),
 				 wr2041n_leds_gpio);
@@ -132,12 +128,20 @@ static void __init wr2041n_setup(void)
 	ath79_register_wmac(art + WR2041N_WMAC_CALDATA_OFFSET, tmpmac);
 
 	ath79_init_mac(tmpmac, mac, 1);
-	ap9x_pci_setup_wmac_led_pin(0, 0);
+	// try setup wlan led pin
+	//ap9x_pci_setup_wmac_led_pin(0, 0);
+	ap9x_pci_setup_wmac_led_pin(0, WR2041N_GPIO_WMAC_LED_WLAN);
+/* important functions related to wmac leds:
+ * void ap9x_pci_setup_wmac_led_pin(unsigned wmac, int pin); ... define wmac led pin, unknown for now
+ * void ap9x_pci_setup_wmac_gpio(unsigned wmac, u32 mask, u32 val);
+ * void ap9x_pci_setup_wmac_leds(unsigned wmac, struct gpio_led *leds, int num_leds);.. define multi wmac leds
+ * 
+ */
 	ap91_pci_init(art + WR2041N_PCIE_CALDATA_OFFSET, tmpmac);
-
 	ath79_setup_ar934x_eth_cfg(AR934X_ETH_CFG_SW_ONLY_MODE);
 
 	ath79_register_mdio(1, 0x0);
+
 	/* LAN */
 	ath79_init_mac(ath79_eth1_data.mac_addr, mac, -1);
 
@@ -157,6 +161,7 @@ static void __init wr2041n_setup(void)
 	ath79_eth0_data.mii_bus_dev = &ath79_mdio1_device.dev;
 
 	ath79_register_eth(0);
+
 
 	ath79_gpio_output_select(WR2041N_GPIO_LED_WAN,
 				 AR934X_GPIO_OUT_LED_LINK4);
