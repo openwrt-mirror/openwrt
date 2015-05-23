793b1314d49332a8b5db00d8cbf4a72889352b58
Syrone Wong <wong.syrone@gmail.com>
2015-05-23 20:01:30 +0800

    mvebu: fan_ctrl: get rid of crontab
    in case some scripts change crontab file.
    close #32

--- target/linux/mvebu/base-files/sbin/fan_ctrl.sh
+++ target/linux/mvebu/base-files/sbin/fan_ctrl.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# loop for every 5 mins
+while [ true ]; do
 
 CPU_TEMP=`cut -c1-2 /sys/class/hwmon/hwmon2/temp1_input`
 DDR_TEMP=`cut -c1-2 /sys/class/hwmon/hwmon1/temp1_input`
@@ -26,3 +28,6 @@ elif [ "$CPU_TEMP" -ge "$CPU_LOW" -o "$DDR_TEMP" -ge "$DDR_LOW" -o "$WIFI_TEMP" 
 else
 	echo "0" > $FAN_CTRL
 fi
+
+/bin/sleep 5m
+done
