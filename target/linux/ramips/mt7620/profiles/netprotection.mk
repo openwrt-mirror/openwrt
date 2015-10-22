3698c6537b8e8277d08d77d29e0f4e53872cd328
João Bernardo Oliveira <jboliveira@netfilter.com.br>
2015-10-22 12:55:48 +0000

    Added packages required by netprotection profile

--- target/linux/ramips/mt7620/profiles/netprotection.mk
+++ target/linux/ramips/mt7620/profiles/netprotection.mk
@@ -7,7 +7,7 @@
 
 define Profile/NETPROTECTION
 	NAME:=Netprotection np-A105003201
-	PACKAGES:=kmod-usb-ohci kmod-usb2
+	PACKAGES:=boost-thread busybox confuse dnsmasq dropbear fstools iptables-mod-nfqueue kmod-gpio-button-hotplug kmod-ipt-nathelper kmod-leds-gpio kmod-ledtrig-usbdev kmod-rt2800-soc kmod-tun kmod-usb-dwc2 kmod-usb-ohci kmod-usb-printer kmod-usb-serial-option kmod-usb-uhci kmod-usb2 libiw libnetfilter-conntrack libnetfilter-queue libprotobuf-c libstdcpp libubox luasocket luci luci-app-ddns luci-app-diag-devinfo luci-app-qos luci-i18n-portuguese-brazilian maccalc miniupnpc miniupnpd mtd odhcp6c odhcpd opkg ppp ppp-mod-pppoe protobuf squid swconfig uci usb-modeswitch usbutils wpad-mini
 endef
 
 define Profile/NETPROTECTION/Description
