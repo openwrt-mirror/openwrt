45ac31d41ef8ffa1a7f8e3c497108ffe7016ff35
Chuanhong Guo <gch981213@gmail.com>
2016-04-29 17:56:23 +0800

    ...

--- package/network/services/hostapd/files/hostapd_wps_led.sh
+++ package/network/services/hostapd/files/hostapd_wps_led.sh
@@ -13,12 +13,14 @@ WPS_LED=$(ls /sys/class/leds | grep -E 'qss|wps' | head -n 1)
 	exit
 }
 
-
 case "$CMD" in
+	WPS-PBC-ACTIVE)
+		led_timer $WPS_LED 300 300
+		;;
 	CTRL-EVENT-EAP-STARTED)
 		led_timer $WPS_LED 120 120
 		;;
-	WPS-TIMEOUT|WPS-SUCCESS|DISCONNECTED|*FAIL*)
+	WPS-TIMEOUT|WPS-SUCCESS|DISCONNECTED)
 		led_off $WPS_LED
 		kill "$(cat "/var/run/hostapd-cli-wpsled-$IFNAME.pid")"
 		;;
