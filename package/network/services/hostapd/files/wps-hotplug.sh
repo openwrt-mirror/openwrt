6002e55e45bfbe7432765a437c702f023d9ba712
Chuanhong Guo <gch981213@gmail.com>
2016-04-29 17:04:52 +0800

    完善WPS LED显示

--- package/network/services/hostapd/files/wps-hotplug.sh
+++ package/network/services/hostapd/files/wps-hotplug.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 . /lib/functions/leds.sh
 
-WPS_LED=$(ls /sys/class/leds | grep -E 'qss|wps' | head -n 1)
-
 if [ "$ACTION" = "pressed" -a "$BUTTON" = "wps" ]; then
-	[ -z $WPS_LED ] || led_timer $WPS_LED 150 150
 	cd /var/run/hostapd
 	for socket in *; do
 		[ -S "$socket" ] || continue
-		hostapd_cli -i "$socket" wps_pbc
+		[ "$(hostapd_cli -i "$socket" wps_pbc)" = "OK" ] && {
+			WPS_LED=$(ls /sys/class/leds | grep -E 'qss|wps' | head -n 1)
+			[ -z "$WPS_LED" ] || {
+				led_timer $WPS_LED 300 300
+				hostapd_cli -i "$socket" -P /var/run/hostapd-cli-wpsled-$socket.pid -a /lib/hostapd_wps_led.sh -B
+			}
+		}
 	done
-	sleep 1
-	[ -z $WPS_LED ] || led_off $WPS_LED
 fi
 
 return 0
